b77910e6af7f57c8c5163524ea7615303c5e4ca0
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-12-18 15:36:46 -0500

    fundamentals practice

---
+++ main.js
@@ -0,0 +1,10 @@
+function evenOrOdd(number) {
+  
+    return number % 2 !== 0  ? 'Odd' : 'Even'
+    
+  }
+  
+  console.log(evenOrOdd(15)) // Odd
+  console.log(evenOrOdd(7)) // Odd
+  console.log(evenOrOdd(0)) // Even
+  
